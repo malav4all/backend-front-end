1342aa188bc0379a92c49df422646f96c6ed58ab
tanishkpandey <pandeytanishk@gmail.com>
2024-03-29 15:15:21 +0530

    login screen change

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import CampaignStatusBox from "./components/CampaignStatusBox";
+import { Box, Grid } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ExecutedCampaignsChart from "./components/Chart/ExecutedCampaignsChart";
+import CampaignRecipientPieChart from "./components/Chart/CampaignRecipientPieChart";
+
 const Dashboard = () => {
-  const { t } = useTranslation();
-  return <div>{t("welcome")}</div>;
+  // const { t } = useTranslation();
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        
+        
+      </Grid>
+    </>
+  );
 };
 
 export default Dashboard;
