fbbfe12a677f0a0a01f6547108b1a202777bb5c6
tanishkpandey <pandeytanishk@gmail.com>
2024-06-13 17:56:50 +0530

    started darkMode for MUI

--- src/screens/Shared/AppDrawer/AppDrawer.tsx
+++ src/screens/Shared/AppDrawer/AppDrawer.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   Collapse,
   List,
@@ -7,15 +8,16 @@ import {
   Tooltip,
   Typography,
   useMediaQuery,
+  Switch,
+  FormControlLabel,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import { RiLogoutCircleRLine } from "react-icons/ri";
-import { useEffect, useState } from "react";
-import React from "react";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { styled } from "@mui/material/styles";
 import history from "../../../utils/history";
 import { isTruthy } from "../../../helpers/methods";
 import { GenerateMenu } from "../../../utils/AuthorizationManager";
@@ -32,10 +34,59 @@ import { useAppDispatch, useAppSelector } from "../../../utils/hooks";
 import LogoutIcon from "../../../assets/icons/Logout.png";
 import ROUTEYE_LOGO from "../../../assets/images/ROUTEYE_LOGO.png";
 import ROUTEYE_LOGO_MINI from "../../../assets/images/ROUTEYE_LOGO_MINI.png";
+import { useTheme } from '../../../utils/ThemeContext';
+
 interface CustomProps {
   setMenuMobileVisible?: Function;
   isActive?: boolean;
 }
+
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  "& .MuiSwitch-switchBase": {
+    margin: 1,
+    padding: 0,
+    transform: "translateX(6px)",
+    "&.Mui-checked": {
+      color: "#fff",
+      transform: "translateX(22px)",
+      "& .MuiSwitch-thumb:before": {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          "#fff"
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
+    width: 32,
+    height: 32,
+    "&::before": {
+      content: "''",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      left: 0,
+      top: 0,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        "#fff"
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+  },
+  "& .MuiSwitch-track": {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+    borderRadius: 20 / 2,
+  },
+}));
 
 const AppDrawer = (props: CustomProps) => {
   const classes = appDrawerStyles;
@@ -46,6 +97,7 @@ const AppDrawer = (props: CustomProps) => {
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
   const dispatch = useAppDispatch();
   const userName = useAppSelector(selectName);
+  const { darkMode, toggleTheme } = useTheme();
 
   useEffect(() => {
     generateAppDrawer();
@@ -337,6 +389,35 @@ const AppDrawer = (props: CustomProps) => {
     );
   };
 
+  const darkModeSection = () => {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+        p={2}
+        borderRadius="10px"
+      >
+        <Box sx={{color: "white"}}>
+          Mode:      
+        </Box>
+        <FormControlLabel
+          control={
+            <MaterialUISwitch
+              checked={darkMode}
+              onChange={toggleTheme}
+              name="darkMode"
+              color="default"
+            />
+          }
+          label=""
+        />
+      </Box>
+    );
+  };
+
   const getAppDrawer = () => {
     return (
       <>
@@ -371,6 +452,7 @@ const AppDrawer = (props: CustomProps) => {
               )}
             </Box>
             <Box sx={classes.menuOptionsHeight}>{getNewMenuOptions()}</Box>
+            <Box>{darkModeSection()}</Box>
             <Box>{getLogoutButton()}</Box>
           </Box>
         </Box>
